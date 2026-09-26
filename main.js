920ef1b9484f7b577d44fac0de627967ead2f6f9
geodle-game <ry2012293239@gmail.com>
2026-09-26 15:18:07 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-import { state, newRun } from './systems/state.js';
+import { state } from './systems/state.js';
 import { render } from './ui/render.js';
 
-newRun();
+// Start at the title screen. newRun() is called when the player
+// clicks Begin, not at boot.
+state.screen = 'mainMenu';
 render();
 
 window.__game = { state };
