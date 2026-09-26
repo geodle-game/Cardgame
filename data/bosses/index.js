e6f513672d24ec7c59df08c57efbb071572b1d37
geodle-game <ry2012293239@gmail.com>
2026-09-26 15:14:06 +0800

    Update index.js

--- data/bosses/index.js
+++ data/bosses/index.js
@@ -1,5 +1,9 @@
 import * as dungeonCore from './dungeon-core.js';
+import * as fallenDrawn from './fallen-drawn.js';
+import * as warden from './warden.js';
 
 export const BOSSES = [
+  fallenDrawn,
+  warden,
   dungeonCore,
 ];
